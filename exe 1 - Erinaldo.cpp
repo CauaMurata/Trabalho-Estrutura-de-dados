a75649295905ba3e83d6a0286cff4014f854c87d
CauaMurata <110571821+CauaMurata@users.noreply.github.com>
2022-08-03 19:34:29 -0300

    Add files via upload

---
+++ exe 1 - Erinaldo.cpp
@@ -0,0 +1,15 @@
+//1. Seja o trecho de código a seguir, quais serão os valores de x, y e *p no comando printf?
+
+#include <stdio.h>
+
+int main(void){
+	int x, y, *p;
+	y = 0;
+	p = &y;
+	x = *p;
+	x = 4;
+	(*p)++;
+	--x;
+	(*p) += x;
+	printf("x=%d  y=%d *p=%d", x, y, *p);
+}
